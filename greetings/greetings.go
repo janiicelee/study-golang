cd37b39909425ba8fc9549f080441d1095108ae1
Yerin Lee <meekukin@gmail.com>
2020-12-01 00:03:53 +0900

    practice #1

---
+++ greetings/greetings.go
@@ -0,0 +1,18 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name.")
+	}
+	// If a name was received, return a value that embeds the name
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
+}
